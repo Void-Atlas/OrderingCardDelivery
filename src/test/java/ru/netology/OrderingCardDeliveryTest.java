49014e3344b23a1b18fcb83225bd768455328725
Daniil Salikov <denielnit@gmail.com>
2026-01-14 17:27:09 +0300

    Update

--- src/test/java/ru/netology/OrderingCardDeliveryTest.java
+++ src/test/java/ru/netology/OrderingCardDeliveryTest.java
@@ -38,6 +38,11 @@ class registrationTest{
         form.$("[data-test-id='phone'] input").setValue("+79000000000");
         form.$("[data-test-id='agreement']").click();
         $$("button").filter(Condition.visible).find(Condition.text("Запланировать")).click();
+        $(Selectors.withText("Успешно!")).should(Condition.visible, Duration.ofSeconds(15));
+        $("[data-test-id='notification']").should(Condition.text("Встреча успешно забронирована на " + planningDate), Duration.ofSeconds(15)).should(Condition.visible);
+        form.$("[data-test-id='date'] input").should(Condition.visible)
+                .press(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE)
+                .setValue(planningDate);
         $$("button").filter(Condition.visible).find(Condition.text("Перепланировать")).click();
         $("[data-test-id='success-notification']").should(Condition.visible, Duration.ofSeconds(15));
         $(Selectors.withText("Успешно!")).should(Condition.visible, Duration.ofSeconds(15));
